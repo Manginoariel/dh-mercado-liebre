cf184f229b3156956bddd4c7dda535c13b640eab
Manginoariel <mangino.a75@gmail.com>
2021-09-01 14:52:58 -0300

    post

--- app.js
+++ app.js
@@ -3,20 +3,22 @@ const app = express();
 
 const path = require("path");
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Se prendió!");
 });
 
-//Metodos de HTTP
-// - GET
-// - POST
-// - PUT
+//Metodos de HTTP- GET - POST - PUT
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views/index.html"));
 });
+
+app.post("/", (req, res) => {
+    res.send("hola mundo");
+});
+
 app.get("/register", (req, res) => {
     res.sendFile(path.join(__dirname, "views/register.html"));
 });
